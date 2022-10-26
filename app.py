90e969d3d247f77b3bc1e65f01481eaf1e6d4f07
yassir <yassiros19@gmail.com>
2022-10-26 11:00:36 +0200

    .......

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask           # import flask
-from flask import render_template
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
-    app.run(debug=True)                     # run the flask app
+    app.run(host="0.0.0.0", debug=True)                     # run the flask app
