07f3e4e95289c3049c2d2e9a665c7ec02069aecc
yassir <yassiros19@gmail.com>
2022-11-02 08:55:20 +0100

    render temp function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask           # import flask
+import datetime
+from flask import Flask, render_template
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())        # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run(host="0.0.0.0",port=5000, debug=True)                     # run the flask app
